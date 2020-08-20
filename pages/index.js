f922051a8955f4c157f364ecd7051211d0a70215
Haruna Oka <oka.haruna@classmethod.jp>
2020-08-20 21:16:32 +0900

    Layoutを追加

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-import { Layout, Page, TextStyle } from '@shopify/polaris';
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
     <Page>
-        <TextStyle variation="positive">
-            Sample app using React and Next.js
-        </TextStyle>
+        <Layout>
+            <EmptyState
+                heading="Discount your products temporarily"
+                action={{
+                    content: 'Select products',
+                    onAction: () => console.log('clicked'),
+                }}
+                image={img}
+            >
+                <p>Select products to change their price temporarily.</p>
+            </EmptyState>
+        </Layout>
     </Page>
 );
 
